faa66dbd29dda01e3f3c087f084c928411faf9a5
pepperflavor <j8747j@naver.com>
2023-06-28 05:25:22 +0900

    fetch() http post요청 보내기

--- http-backend/src/App.js
+++ http-backend/src/App.js
@@ -16,22 +16,30 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.py4e.com/api/films");
+      // 스타워즈 : https://swapi.py4e.com/api/films
+      const response = await fetch(
+        "https://react-http-944a9-default-rtdb.firebaseio.com/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Something Went Wrong...!!");
       }
+
+      // 돌려받은 응답 json으로 파싱
       const data = await response.json();
 
-      const transformMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -42,14 +50,29 @@ function App() {
     fetchMovieHandler();
   }, []);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  // AddMovie 컴포넌트에서 입력한 영화에 대한 객체데이터 갖고오기
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-944a9-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json", // 보통 API 요청을 보낼때 어떤 컨텐츠를 담아서 보내는지 표기해줌
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
     <React.Fragment>
       <section>
         <AddMovie onAddMovie={addMovieHandler}></AddMovie>
+      </section>
+      <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
